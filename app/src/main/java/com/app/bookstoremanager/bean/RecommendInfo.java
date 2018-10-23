e84b57d1d13c5d1c504c6c0e279e30d5173243f3
ming <1719763402@qq.com>
2018-10-23 17:56:11 +0800

    测试首页

---
+++ app/src/main/java/com/app/bookstoremanager/bean/RecommendInfo.java
@@ -0,0 +1,27 @@
+package com.app.bookstoremanager.bean;
+
+public class RecommendInfo {
+
+    private String name;
+    private String imageUrl;
+
+    //这里注意做一个无参数的构造器
+    public RecommendInfo() {
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
--- app/src/main/java/com/app/bookstoremanager/view/fragment/BookCityFragment.java
+++ app/src/main/java/com/app/bookstoremanager/view/fragment/BookCityFragment.java
@@ -1,19 +1,24 @@
 package com.app.bookstoremanager.view.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
 import com.app.bookstoremanager.R;
 import com.app.bookstoremanager.base.BaseFragment;
+import com.app.bookstoremanager.bean.RecommendInfo;
 import com.app.bookstoremanager.utils.ToastUtil;
 import com.app.bookstoremanager.view.adapter.MyLoader;
+import com.app.bookstoremanager.view.adapter.RecommendAdapter;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.Transformer;
 import com.youth.banner.listener.OnBannerListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -22,8 +27,38 @@ public class BookCityFragment extends BaseFragment implements OnBannerListener {
     @BindView(R.id.banner)
     Banner banner;
 
+    private ArrayList<RecommendInfo> birdList;
+    private RecommendAdapter myAdapter;
+
+    @BindView(R.id.recycleview)
+    RecyclerView mRecycleView;
+
     private ArrayList<String> list_path;
     private ArrayList<String> list_title;
+
+    private final String names[] = {
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow"
+    };
+    private final String imageUrls[] = {
+            "http://hawksaloft.org/wp-content/uploads/2012/08/614612316_20090805-_mg_3411-rufous-hummingbird-5x7.jpg",
+            "http://www.gregscott.com/gjs_2007_spring/hummingbird/20070311_1948_100_0560.rufous_humminbird.jpg",
+            "http://mosthdwallpapers.com/wp-content/uploads/2016/06/Flying-Hummingbird-Pictures.jpg",
+            "https://wallpapercave.com/wp/alkKAoC.jpg",
+            "http://mosthdwallpapers.com/wp-content/uploads/2016/06/Gorgeous-Hummingbird-Wallpapers-For-Desktop.jpg",
+            "http://naturecanada.ca/wp-content/uploads/2014/07/Ruby-throat-Hummingbird-shutterstock_1953533.jpg",
+            "http://images5.fanpop.com/image/photos/26100000/Hummingbird-hummingbirds-26167630-1024-740.jpg",
+            "https://farm5.staticflickr.com/4065/4698051727_5024cd4e6c_b.jpg",
+            "http://mosthdwallpapers.com/wp-content/uploads/2016/06/Beautiful-Hummingbird-HD-Photography.jpg",
+    };
+
 
     @Override
     protected View initView(Bundle savedInstanceState) {
@@ -70,6 +105,26 @@ public class BookCityFragment extends BaseFragment implements OnBannerListener {
                 .setOnBannerListener(this)
                 //必须最后调用的方法，启动轮播图。
                 .start();
+
+        mRecycleView.setHasFixedSize(true);
+        mRecycleView.setLayoutManager(new GridLayoutManager(mActivity, 3));
+        birdList = init();
+        myAdapter = new RecommendAdapter(mActivity,birdList);
+        mRecycleView.setAdapter(myAdapter);
+
+    }
+
+    private ArrayList<RecommendInfo> init() {
+        ArrayList<RecommendInfo> birds = new ArrayList<>();
+
+        for (int i = 0; i < names.length; ++i) {
+            RecommendInfo bird = new RecommendInfo();
+            bird.setImageUrl(imageUrls[i]);
+            bird.setName(names[i]);
+            birds.add(bird);
+        }
+        return birds;
+
     }
 
     //轮播图的监听方法
