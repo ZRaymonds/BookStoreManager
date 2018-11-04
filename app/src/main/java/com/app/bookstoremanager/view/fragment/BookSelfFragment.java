52825fd4dd85d5b6f03d6c77a03a8a7fc90e6800
ZRaymond <1719763402@qq.com>
2018-11-04 11:00:41 +0800

    加入管理权限

--- app/src/main/java/com/app/bookstoremanager/view/fragment/BookSelfFragment.java
+++ app/src/main/java/com/app/bookstoremanager/view/fragment/BookSelfFragment.java
@@ -1,8 +1,13 @@
 package com.app.bookstoremanager.view.fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -16,6 +21,7 @@ import com.app.bookstoremanager.bean.BookBean;
 import com.app.bookstoremanager.common.view.GridSpacingItemDecoration;
 import com.app.bookstoremanager.model.impl.BookDao;
 import com.app.bookstoremanager.presenter.ShelfPresenter;
+import com.app.bookstoremanager.utils.ToastUtil;
 import com.app.bookstoremanager.view.ShelfContact;
 import com.app.bookstoremanager.view.activity.ImportBookActivity;
 import com.app.bookstoremanager.view.activity.ReadBookActivity;
@@ -125,9 +131,35 @@ public class BookSelfFragment extends BaseFragment implements ShelfBookAdapter.O
                 allSelectBook(mAdapter.getBookMap());
                 break;
             case R.id.add_book:
-                Intent intent = new Intent(mActivity, ImportBookActivity.class);
-                mActivity.startActivity(intent);
+                if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                }  else {
+                    readCard();
+                }
                 break;
+        }
+    }
+
+    private void readCard() {
+        try {
+            Intent intent = new Intent(mActivity, ImportBookActivity.class);
+            mActivity.startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    readCard();
+                } else {
+                    ToastUtil.show(mActivity, "您取消了授权！");
+                }
+                break;
+            default:
         }
     }
 
